15cfdad40819fcdae3ef88a7f0b2ad56e2e655cb
Olver Eduardo Ramos <olver.ramos@er-technology.net>
2025-12-16 17:25:34 -0500

    Ajuste Debug

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,7 @@ DATABASE_NAME = os.getenv('DATABASE_NAME','requisitiondb')
 DATABASE_URL = os.getenv('DATABASE_URL','localhost')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = str2bool(os.environ.get('DEBUG', True))
-DEBUG = True
+DEBUG = False
 try:
     from .settings_debug import *
 except:
