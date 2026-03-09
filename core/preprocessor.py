9716e72e6bc027dbb1bf3d4965c3f8ff642119f7
Olver Eduardo Ramos <olver.ramos@er-technology.net>
2026-03-09 10:08:53 -0500

    Menú

--- core/preprocessor.py
+++ core/preprocessor.py
@@ -1,57 +1,25 @@
-import datetime
-from django.conf import settings
 from modules.auths.models import Account
+from modules.menu.models import Option
+from django.conf import settings
+import datetime as dt
 
 def generic_variables(request):
     account = None
     if request.user.is_authenticated:
         account = Account.getAccount(request.user)
     
-    menu_list = {
-        'admin': [
-            {
-                'url': 'auth_accounts',
-                'title': 'Usuarios'
-            },
-            # {
-            #     'url': 'parameters_documentclass',
-            #     'title': 'Clase Documentos'
-            # },
-            # {
-            #     'url': 'parameters_ramo_field',
-            #     'title': 'Campos de Ramo'
-            # },
-            # {
-            #     'url': 'parameters_ramos',
-            #     'title': 'Ramos'
-            # },
-            # {
-            #     'url': 'base_applicants',
-            #     'title': 'Solicitantes'
-            # },
-            # {
-            #     'url': 'base_takers',
-            #     'title': 'Tomadores'
-            # },
-        ],
-        'operative': [
-            # {
-            #     'url': 'operative_requests',
-            #     'title': 'Solicitudes'
-            # },
-        ],
-    }
+    menu_list = Option.getMenu(account)
 
     return {    
-                'ACCOUNT': account,
-                'CURRENT_YEAR': datetime.date.today().strftime("%Y"),
-                'TODAY': datetime.date.today().strftime("%Y-%m-%d"),
-                'APP_NAME': settings.APP_NAME,
-                'COMERCIAL_APP_NAME': settings.COMERCIAL_APP_NAME,
-                'APP_VERSION': settings.APP_VERSION,
-                'APP_URL': settings.APP_URL,
-                'STATIC_URL': settings.STATIC_URL,
-                'ENVIRONMENT': settings.ENVIRONMENT,
-                'ENTERPRISE': settings.ENTERPRISE,
-                'MENU': menu_list
-            }
+        'ACCOUNT': account,
+        'CURRENT_YEAR': dt.date.today().strftime("%Y"),
+        'TODAY': dt.date.today().strftime("%Y-%m-%d"),
+        'APP_NAME': settings.APP_NAME,
+        'COMERCIAL_APP_NAME': settings.COMERCIAL_APP_NAME,
+        'APP_VERSION': settings.APP_VERSION,
+        'APP_URL': settings.APP_URL,
+        'STATIC_URL': settings.STATIC_URL,
+        'ENVIRONMENT': settings.ENVIRONMENT,
+        'ENTERPRISE': settings.ENTERPRISE,
+        'MENU': menu_list
+    }
